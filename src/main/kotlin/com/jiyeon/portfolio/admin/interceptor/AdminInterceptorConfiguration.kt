e8c51482a9a71e50d11df6d111266e7643884ecc
jiyeon0926 <yeoni9094@naver.com>
2025-03-27 18:59:18 +0900

    feat: AdminInterceptor 개발
    - AdminInterceptor
    - AdminInterceptorConfiguration

--- src/main/kotlin/com/jiyeon/portfolio/admin/interceptor/AdminInterceptorConfiguration.kt
+++ src/main/kotlin/com/jiyeon/portfolio/admin/interceptor/AdminInterceptorConfiguration.kt
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class AdminInterceptorConfiguration(val adminInterceptor: AdminInterceptor) : WebMvcConfigurer {
 
     override fun addInterceptors(registry: InterceptorRegistry) {
-
+        registry.addInterceptor(adminInterceptor)
+            .addPathPatterns("/admin/**")
+            .excludePathPatterns("/assets/**", "/css/**", "/js/**", "/h2**")
     }
 }
