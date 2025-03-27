5f64c84d5d99a88fa76f9129df9903be3319aa40
jiyeon0926 <yeoni9094@naver.com>
2025-03-27 17:32:10 +0900

    feat: Exception과 Advice 개발

--- src/main/kotlin/com/jiyeon/portfolio/admin/exception/AdminException.kt
+++ src/main/kotlin/com/jiyeon/portfolio/admin/exception/AdminException.kt
@@ -7,3 +7,11 @@ abstract class AdminException(
     message: String) : RuntimeException(message) {
         val httpStatus: HttpStatus = httpStatus
 }
+
+class AdminBadRequestException(message: String) : AdminException(
+    HttpStatus.BAD_REQUEST,
+    message)
+
+class AdminInternalServerErrorException(message: String) : AdminException(
+    HttpStatus.INTERNAL_SERVER_ERROR,
+    message)
