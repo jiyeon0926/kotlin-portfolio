4fdd306b45f94b778c06c01567a66979c063c0cc
jiyeon0926 <yeoni9094@naver.com>
2025-04-01 18:19:35 +0900

    feat: 삽입, 삭제 API 개발 - 연관관계 N:M

---
+++ src/main/kotlin/com/jiyeon/portfolio/admin/context/project/controller/AdminProjectSkillApiController.kt
@@ -0,0 +1,26 @@
+package com.jiyeon.portfolio.admin.context.project.controller
+
+import com.jiyeon.portfolio.admin.context.project.form.ProjectSkillForm
+import com.jiyeon.portfolio.admin.context.project.service.AdminProjectSkillService
+import com.jiyeon.portfolio.admin.data.ApiResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/admin/api/projects/skills")
+class AdminProjectSkillApiController(private val adminProjectSkillService: AdminProjectSkillService) {
+
+    @PostMapping
+    fun postProjectSkill(@RequestBody form: ProjectSkillForm): ResponseEntity<Any> {
+        adminProjectSkillService.save(form)
+
+        return ApiResponse.successCreate()
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteProjectSkill(@PathVariable("id") id: Long): ResponseEntity<Any> {
+        adminProjectSkillService.deleteProjectSkill(id)
+
+        return ApiResponse.successDelete()
+    }
+}
